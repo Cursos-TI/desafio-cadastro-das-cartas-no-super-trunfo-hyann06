bc4468cc6783f0ab7c7e9a27f2a81e309a644578
hyann06 <hyan.abreu13@gmail.com>
2025-05-15 01:58:12 +0000

    Criei parte da 1º carta (DANDO ERRO)

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,16 +7,52 @@
 //Teste larissa
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    printf("BEM VINDO AO JOGO SUPER TRUNFO!\n");    //Texto de boas avindas
+    printf("\n");
+    printf(">Começe cadastrando suas cartas<\n");   //Solicitação pra cadastrar as cartas
     
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("\n");   //Espaço entre o texto de boas vindas e o cadastramento das cartas
+
+
+    char letra;
+    char nome[30];
+    char codigo[30];
+    int populacao;
+    int turistico;
+    float area;
+    float pib;
+
+    printf("    Carta 1     \n");   //Informações da 1º carta
+
+    printf("Digite o nome do estado: ");
+    scanf("%s", nome);
+
+    printf("Digite o codigo da carta: ");
+    scanf("%s", codigo);
+
+    printf("Digite a primeira letra do estado escolhido: ");    //Por que não consigo preencher essa caixa, e nem quebrar ela pra outra linha?
+    scanf("%c", letra);
+
+    printf("Digite a população do estado: ");
+    scanf("%d", &populacao);
+
+    printf("Digite a área do estado: ");
+    scanf("%f", &area);
+
+    printf("Digite o PIB do estado: ");
+    scanf("%f", &pib);
+
+    printf("Digite a quantidade de pontos turisticos do estado: ");
+    scanf("%d", &turistico);
+    
+    printf("\n");   //muito errado, REVER 
+    printf("Informações da 1º carta: \n");
+    printf("\n");
+
+    printf("Nome do estado: %s \n");
+
+
 
     return 0;
 }
