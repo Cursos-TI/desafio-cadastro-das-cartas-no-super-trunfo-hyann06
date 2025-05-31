3fcc56df128580785413bf96e165b6142b72951f
hyann06 <hyan.abreu13@gmail.com>
2025-05-31 01:53:55 +0000

    1º tentativa da adição super poder/comparar Carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,7 +9,7 @@ int main() {
     printf("\n");   
 
 
-    int populacao, POPULACAO;
+    unsigned long int populacao, POPULACAO;
     int turistico, TURISTICO;
     float area, AREA;
     float pib, PIB;
@@ -19,7 +19,9 @@ int main() {
     char nome[30], NOME[30];
     char codigo[30], CODIGO[30];
 
-    printf("    >Carta 1<     \n");   //Informações da 1º carta
+
+    //INFORMAÇÕES DA PRIMEIRA CARTA
+    printf("    >Carta 1<     \n");  
     printf("\n");
 
     printf("Escolha uma letra de 'A' a 'H': ");    //Letra da carta
@@ -32,7 +34,7 @@ int main() {
     scanf(" %[^\n]", nome);
 
     printf("Digite a população do estado: ");
-    scanf("%d", &populacao);
+    scanf("%lu", &populacao);
 
     printf("Digite a área do estado: ");
     scanf("%f", &area);
@@ -43,18 +45,18 @@ int main() {
     printf("Digite a quantidade de pontos turisticos do estado: ");
     scanf("%d", &turistico);
 
-    denp = (float)(populacao / area);
-    pibper = (float)(pib / populacao);
+    denp = (float)(populacao / area);   //DENSIDADE POPULACIONAL DA PRIMEIRA CARTA
+    pibper = (float)(pib / populacao);  //PIB PERCAPTO DA PRIMEIRA CARTA
 
     printf("\n");
     printf("\n");
     
-
-    printf("    >Carta 2<     \n");     //informações da 2º carta
+    //INFORMAÇÕES DA SEGUNDA CARTA 
+    printf("    >Carta 2<     \n");     
     printf("\n");
 
 
-    printf("Escolha uma letra de 'A' a 'H' que não tenha escolhido antes:");
+    printf("Escolha uma letra de 'A' a 'H' que não tenha escolhido antes: ");
     scanf(" %c", &ESTADO);
     
     printf("Digite o códico da carta, do '01' ao '04': ");
@@ -75,8 +77,8 @@ int main() {
     printf("Digite a quantidade de pontos turisticos do estado: ");
     scanf("%d", &TURISTICO);
 
-    DENP = (float)(POPULACAO / AREA);
-    PIBPER = (float)(PIB / POPULACAO);
+    DENP = (float)(POPULACAO / AREA);   //DENSIDADE POPULACIONAL DA SEGUNDA CARTA
+    PIBPER = (float)(PIB / POPULACAO);  //PIB PERCAPTO DA SEGUNDA CARTA
 
 
     printf("\n");
@@ -108,11 +110,20 @@ int main() {
     printf("Densidade Populacional: %.2F \n", DENP);
     printf("PIB Percapto: %.2f \n", PIBPER);
 
-
-
+    printf("\n");
+    printf(">COMPARANDO OS ATRIBUTOS< \n");
+    printf("\n");
     
+    printf("Carta 1 (1) / Carta 2 (0)\n\n");
 
-
+    printf("População: (%d) venceu!\n", populacao>POPULACAO);
+    printf("Área: (%d) venceu!\n", area>AREA);
+    printf("PIB: (%d) venceu!\n", pib>PIB);
+    printf("Pontos Turísticos: (%d)venceu!\n", turistico>TURISTICO);
+    printf("Densidade Populacional: (%d) venceu!\n", denp<DENP);
+    printf("PIB Percapto: (%d) venceu!\n", pibper>PIBPER);
+    
+    //Printf("Super Poder: %d venceu!", superpoder>SUPERPODER);
 
     return 0;
 }
